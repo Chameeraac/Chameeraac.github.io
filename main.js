fe352d5a20e9559ad6a59fcebb2746e6d7b43073
Chameeraac <99298576+Chameeraac@users.noreply.github.com>
2023-06-07 11:50:47 +1200

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,18 @@ navToggle.addEventListener('click', ()=> {
     primaryHeader.toggleAttribute("data-overlay");
 });
 
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false, // dots enabled 1280px and up
+    },
+  },
+});
+
 // Script for slideshow to be animated on main page
 let slideIndex = 0;
 showSlides();
